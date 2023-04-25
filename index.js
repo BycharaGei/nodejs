d214b055d930b3c95afb18df69b5107fd685af24
BycharaGei <102876659+BycharaGei@users.noreply.github.com>
2023-04-25 18:22:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const http = require('http');
-const PORT = 3000;
+const net = require('net');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+const server = net.createServer((socket) => {
+  console.log('Client connected');
+
+  socket.on('data', (data) => {
+    console.log(`Received message: ${data}`);
+  });
+
+  socket.on('end', () => {
+    console.log('Client disconnected');
+  });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+const PORT = 3000;
+
+server.listen(PORT, 'nodejs-production-3117.up.railway.app',  () => {
+  console.log('sa as');
 });
